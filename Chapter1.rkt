61ff31effbb53595cc7619d835b2a9368574096f
dustinburda <dustin99wii@gmail.com>
2025-05-25 23:51:52 -0400

    Chapter 1

---
+++ Chapter1.rkt
@@ -0,0 +1,24 @@
+#lang scheme
+(+ 137 349)
+(- 1000 334)
+(* 5 99)
+(+ 21 35 12 7)
+(+ (* 3 5) (- 10 6))
+
+(+ (* 3
+      (+ (* 2 4)
+         (+ 3 5)))
+   (+ (- 10 7)
+      6))
+
+
+(define size 2)
+(* 5 size)
+
+
+
+(define pi 3.14)
+(define radius 10)  
+
+(define circumference (* 2 pi radius))
+circumference
